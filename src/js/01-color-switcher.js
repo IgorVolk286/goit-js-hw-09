aaddf67d33875c3a80c3eb2314a69feb61097349
IgorVolk286 <125205247+IgorVolk286@users.noreply.github.com>
2023-07-04 23:21:25 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const refs = {
+  buttonStart: document.querySelector('button[data-start]'),
+  buttonStop: document.querySelector('button[data-stop]'),
+};
 
+refs.buttonStart.addEventListener('click', onButtonStartClick);
+refs.buttonStop.addEventListener('click', onButtonStopClick);
+
+let colorId = null;
+
+function onButtonStartClick(event) {
+  console.log(event);
+  event.target.disabled = true;
+  refs.buttonStop.disabled = false;
+  colorId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  //   console.log(colorId);
+}
+
+function onButtonStopClick(event) {
+  event.target.disabled = true;
+  clearInterval(colorId);
+  document.body.style.backgroundColor = '#ffffff';
+  refs.buttonStart.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
